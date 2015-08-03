9f620efb0a64891d95a1e463ea1ac21330eddd80
James Middlemiss <middlemissj.usa@gmail.com>
2015-08-03 11:33:28 -0400

    adjusted routes for user index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-   root to: "home#index"
+   root to: "users#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

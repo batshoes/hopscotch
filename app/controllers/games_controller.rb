14aadfa9e0c13fdfdac3812342e01f22bc0fb8f3
Gwen Larson <glarson284@gmail.com>
2015-08-04 16:40:22 -0400

    this closed #1

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,7 +16,6 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     @game.user_id = current_user.id
-    # binding.pry
     if @game.valid?
       @game.save
       redirect_to games_path(current_user)

401accfaad4dcce6bb26e43c3770a0e873f7f504
James Middlemiss <middlemissj.usa@gmail.com>
2015-10-18 11:46:28 -0400

    altered users/index to view last created

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_many :games
   has_many :comments, dependent: :destroy
 
-def self.connect_to_linkedin(auth, signed_in_resource=nil)
+  def self.connect_to_linkedin(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     if user
       return user
@@ -20,10 +20,8 @@ def self.connect_to_linkedin(auth, signed_in_resource=nil)
                             provider:auth.provider,
                             uid:auth.uid,
                             email:auth.info.email,
-                            password:Devise.friendly_token[0,20],
-                          )
+                            password:Devise.friendly_token[0,20],)
       end
-
     end
   end   
 

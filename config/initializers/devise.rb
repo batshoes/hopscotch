5b4f2c301ba37bbb6b12301b3207e7bdd770eee4
James Middlemiss <middlemissj.usa@gmail.com>
2015-10-19 18:03:15 -0400

    added environment variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,9 +239,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   require 'omniauth-linkedin'
-  config.omniauth :linkedin, '77x9c91eitt3i2', '8Tx8KLNfYw5nunOy'
+  config.omniauth :linkedin, ENV['LINKEDIN_PUBLIC'], ENV['LINKEDIN_SECRET']
   require 'omniauth-facebook'
-  config.omniauth :facebook, '1611811912397878', 'ecd936946c7fecf84020116ac762aa28', scope: 'email', info_fields: 'email, name'
+  config.omniauth :facebook, ENV['FACEBOOK_PUBLIC'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'email, name'
 
 
   # ==> Warden configuration
